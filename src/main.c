96e3aaf29bec04e3e614c969cfaf097620cace23
Chibin Zhang <chibinzhang@hotmail.com>
2021-05-16 13:21:56 +0800

    Rename file name cuckoo.c to table.c

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "cuckoo.h"
+#include "table.h"
 #include "xxhash.h"
 #include "types.h"
 
--- src/table.c
+++ src/table.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "cuckoo.h"
+#include "table.h"
 #include "types.h"
 #include "xxhash.h"
 
