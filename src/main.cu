ea6058d4c4978b163835371f96f05ee85cdcbcf9
Chibin Zhang <chibinzhang@hotmail.com>
2021-05-18 15:41:27 +0000

    Add simple hello world kernel

--- src/main.cu
+++ src/main.cu
@@ -1,9 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "cuda.h"
+
 #include "HostTable.h"
-#include "xxHash.h"
 #include "Types.h"
+#include "xxHash.h"
+
+__global__ void hello() {
+  printf("blockIdx.x=%d/%d blocks, threadIdx.x=%d/%d threads\n", blockIdx.x,
+         gridDim.x, threadIdx.x, blockDim.x);
+}
 
 int main() {
   auto t = HostTable(10, 2);
@@ -12,6 +19,17 @@ int main() {
     t.insert(rand());
     t.write(stdout);
     putchar('\n');
+  }
+
+  hello<<<1, 1024>>>();
+
+  cudaDeviceSynchronize();
+
+  cudaError_t err = cudaGetLastError(); // Get error code
+
+  if (err != cudaSuccess) {
+    printf("CUDA Error: %s\n", cudaGetErrorString(err));
+    exit(-1);
   }
 
   return 0;
