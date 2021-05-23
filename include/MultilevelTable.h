3fd8cecf8ae5beed8f7bc45e0a5d5f3c8ab27662
Chibin Zhang <chibinzhang@hotmail.com>
2021-05-23 13:49:23 +0000

    Bucketed insert with shared memory working!

--- include/MultilevelTable.h
+++ include/MultilevelTable.h
@@ -11,12 +11,11 @@ struct MultilevelTable : public DeviceTable {
   u32 bucketCapacity;
   u32 *bucketSize;
   u32 *bucketData;
-  /// Seed used in respective bucket hash function
-  u32 *bucketSeed;
 
   MultilevelTable() = default;
 
-  MultilevelTable(u32 dim, u32 len, u32 bucket) : bucket(bucket) {
+  MultilevelTable(u32 dim, u32 len, u32 bucket, u32 bucketCapacity)
+      : bucket(bucket), bucketCapacity(bucketCapacity) {
     this->dim = dim;
     this->len = len;
     threshold = 4 * bit_width(dim * len);
@@ -24,12 +23,16 @@ struct MultilevelTable : public DeviceTable {
 
     cudaMallocManaged(&val, sizeof(u32) * dim * len * bucket);
     cudaMallocManaged(&seed, sizeof(u32) * dim * bucket);
+    cudaMallocManaged(&bucketSize, sizeof(u32) * bucket);
+    cudaMallocManaged(&bucketData, sizeof(u32) * bucketCapacity * bucket);
+
     cudaMemset(val, -1, sizeof(u32) * dim * len * bucket);
     randomize(seed, dim * bucket);
   }
 
+  /// Destructor of `DeviceTable` will free `val` and `seed`
   ~MultilevelTable() {
-    cudaFree(val);
-    cudaFree(seed);
+    cudaFree(bucketSize);
+    cudaFree(bucketData);
   }
 };
--- src/MultilevelTable.cu
+++ src/MultilevelTable.cu
@@ -2,6 +2,7 @@
 
 #include "cuda.h"
 #include "cuda_runtime.h"
+#include "device_functions.h"
 #include "device_launch_parameters.h"
 
 #include "Common.h"
@@ -18,12 +19,19 @@ __global__ void bucketInput(MultilevelTable *t, u32 *array, u32 n) {
     if (old < t->bucketCapacity) {
       t->bucketData[b * t->bucketCapacity + old] = array[id];
     } else {
-      printf("Bucket overflow!\n");
+      printf("Bucket overflow! %u\n", blockIdx.x);
     }
   }
 }
 
 __global__ void bucketInsert(MultilevelTable *t) {
+  // Declare fixed size shared memory
+  __shared__ u32 local[3 * 128];
+  // Initialize shared memory
+  for (u32 i = threadIdx.x; i < 3 * 128; i += blockDim.x)
+    local[i] = empty;
+  __syncthreads();
+
   u32 bid = blockIdx.x;
   u32 tid = threadIdx.x;
 
@@ -31,14 +39,19 @@ __global__ void bucketInsert(MultilevelTable *t) {
     u32 v = t->bucketData[bid * t->bucketCapacity + tid];
 
     for (u32 i = 0; i < t->threshold && v != empty; i += 1) {
-      u32 b = i % t->dim;
-      u32 key = xxhash(t->seed[b], v) % t->len;
-      v = atomicExch(&t->val[b * t->len + key], v);
+      u32 d = i % t->dim;
+      u32 key = xxhash(t->seed[d], v) % t->len;
+      v = atomicExch(&local[d * t->len + key], v);
     }
 
     // Record number of collisions
     if (v != empty) {
       atomicAdd(&t->collision, 1);
+    } else {
+      // Copy value from shared memory to global memory
+      for (u32 i = threadIdx.x; i < 3 * 128; i += blockDim.x) {
+        t->val[bid * t->len * t->dim + i] = local[i];
+      }
     }
   }
 }
@@ -46,6 +59,30 @@ __global__ void bucketInsert(MultilevelTable *t) {
 __global__ void bucketLookup(MultilevelTable *T) {}
 
 void test() {
-  auto t = new MultilevelTable();
+  u32 dim = 3;
+  u32 len = 128;
+  u32 bucket = 1 << 17;
+  u32 bucketCapacity = 256;
+  u32 numEntries = 1 << 24;
+  u32 numThreads = 1024;
+  u32 entryBlocks = numEntries / numThreads;
 
+  auto t = new MultilevelTable(dim, len, bucket, bucketCapacity);
+
+  u32 *array;
+  cudaMallocManaged(&array, sizeof(u32) * numEntries);
+  randomize(array, numEntries);
+  syncCheck();
+
+  bucketInput<<<entryBlocks, numThreads>>>(t, array, numEntries);
+  syncCheck();
+
+  bucketInsert<<<bucket, bucketCapacity>>>(t);
+  syncCheck();
+
+  printf("Total number of collisions: %u\n", t->collision);
+  cudaFree(array);
+  delete t;
+
+  syncCheck();
 }
